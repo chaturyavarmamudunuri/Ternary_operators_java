4f62e906f22d17e84464e94027cb6c4fdc589645
chaturyavarmamudunuri <chaturyavarmamudunuri@gmail.com>
2026-07-17 23:18:42 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,12 @@
+public class TernaryOperator {
+
+    public static void main(String args[]) {
+
+        int x = 10, y = 12;
+        int z;
+
+        z = (x > y) ? x : y;
+
+        System.out.println("Z = " + z);
+    }
+}
